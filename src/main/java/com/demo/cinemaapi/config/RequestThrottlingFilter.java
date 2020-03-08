1a8fe70ddea5ebf9291a4634605305a8fb8eadb7
Mateusz Piasek <mpiasek92@gmail.com>
2020-03-08 20:17:11 +0100

    Disabled RequestThrottlingFilter for unauthorized requests

--- src/main/java/com/demo/cinemaapi/config/RequestThrottlingFilter.java
+++ src/main/java/com/demo/cinemaapi/config/RequestThrottlingFilter.java
@@ -1,7 +1,10 @@
 package com.demo.cinemaapi.config;
 
 import io.github.bucket4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.*;
@@ -16,9 +19,17 @@ public class RequestThrottlingFilter implements Filter {
 
     private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
 
+    private final Logger logger = LoggerFactory.getLogger(RequestThrottlingFilter.class);
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            //let Spring Security process not authenticated requests
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+        String username = authentication.getName();
 
         Bucket requestBucket = this.buckets.computeIfAbsent(username, key -> getUserBucket());
 
